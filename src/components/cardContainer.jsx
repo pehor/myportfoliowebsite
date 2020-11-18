1574dfc8dd852985261466b2356eaa6e0e29f20d
Peter Horvath <>
2020-11-18 22:51:00 +0000

    Adding image copyright and see all text

--- src/components/cardContainer.jsx
+++ src/components/cardContainer.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 import { cx } from '../lib/cssTools'
 
 export default function CardContainer (props) {
-  const { children, className, preHeaderContent } = props
+  const { children, className, preHeaderContent, postHeaderContent } = props
 
   return (
     <div className={ cx(styles.cardContainer, className) }>
       <div className={ styles.preHeaderStyles }> { preHeaderContent || '' }</div>
       { children }
+      <div className={ styles.postHeaderStyles }> { postHeaderContent || '' }</div>
     </div>
   )
 }
--- src/components/topSkillsCard.jsx
+++ src/components/topSkillsCard.jsx
@@ -4,10 +4,19 @@ import styles from './topSkillsCard.module.scss'
 import { cx } from '../lib/cssTools'
 import commonStyles from '../commonStyles/tools.scss'
 
+function TopSkillsCardPostElement () {
+  return (
+    <>
+      <div className={ commonStyles.right }>SEE ALL SKILLS &gt;&gt;</div>
+      <div className={ styles.imageCopyright }>Image by Jakub Dziubak (Unsplash)</div>
+    </>
+  )
+}
+
 export default function TopSkillsCard (props) {
   return (
     <>
-      <CardContainer className={ styles.topSkillsCard } preHeaderContent={'TOP SKILLS'}>
+      <CardContainer className={ styles.topSkillsCard } preHeaderContent={ 'TOP SKILLS' } postHeaderContent={ TopSkillsCardPostElement() }>
         <div className={ cx(commonStyles.left, styles.topSkillsText)}>
           { props.texts.topSkills.left.map((skill, index) => {
             return (
